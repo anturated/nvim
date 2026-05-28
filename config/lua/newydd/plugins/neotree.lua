58ec0052cfd32b89069f13c1611762d9b00098f8
Desant <desant@anturated.dev>
2026-05-29 01:04:42 +0200

    feat: add better newotree icons

--- config/lua/newydd/plugins/neotree.lua
+++ config/lua/newydd/plugins/neotree.lua
@@ -21,19 +21,43 @@ return {
 
         default_component_configs = {
           indent = { with_expanders = true },
-          -- git_status = {
-          --   symbols = {
-          --     added = " ",
-          --     modified = "",
-          --     deleted = " ",
-          --     renamed = " ",
-          --     untracked = " ",
-          --     ignored = "",
-          --     unstaged = " ",
-          --     staged = "",
-          --     conflict = " ",
-          --   },
-          -- },
+          filesystem = {
+            filtered_items = {
+              visible = true,
+              hide_dotfiles = false,
+            },
+            use_libuv_file_watcher = true,
+          },
+          -- NOTE: idk if this is better than what i had with web icons but i'm too tired for now
+          icon = {
+            provider = function(icon, node) -- setup a custom icon provider
+              local text, hl
+              local mini_icons = require("mini.icons")
+              if node.type == "file" then -- if it's a file, set the text/hl
+                text, hl = mini_icons.get("file", node.name)
+              elseif node.type == "directory" then -- get directory icons
+                text, hl = mini_icons.get("directory", node.name)
+                -- only set the icon text if it is not expanded
+                if node:is_expanded() then
+                  text = nil
+                end
+              end
+
+              -- set the icon text/highlight only if it exists
+              if text then
+                icon.text = text
+              end
+              if hl then
+                icon.highlight = hl
+              end
+            end,
+          },
+          kind_icon = {
+            provider = function(icon, node)
+              local mini_icons = require("mini.icons")
+              icon.text, icon.highlight = mini_icons.get("lsp", node.extra.kind.name)
+            end,
+          },
         },
       }
 
