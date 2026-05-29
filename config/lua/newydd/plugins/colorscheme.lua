51d6a22a9aa4f27e0419587805820720c84bd243
Desant <desant@anturated.dev>
2026-05-29 17:25:28 +0200

    feat: add accent switcher

--- config/lua/newydd/plugins/colorscheme.lua
+++ config/lua/newydd/plugins/colorscheme.lua
@@ -3,6 +3,8 @@ return {
     "evergarden-nvim",
     priority = 1000,
     after = function()
+      local evergarden = require("evergarden")
+
       local opts = {
         editor = { transparent_background = true },
         theme = {
@@ -11,8 +13,20 @@ return {
         },
       }
 
-      require("evergarden").setup(opts)
+      evergarden.setup(opts)
       vim.cmd.colorscheme("evergarden")
+
+      vim.api.nvim_create_user_command("NewyddAccent", function(opts_)
+        local accent = opts_.args
+        opts.theme.accent = accent
+
+        require("evergarden").setup(opts)
+        vim.cmd("colorscheme evergarden")
+        vim.notify("Switched accent to " .. accent)
+      end, {
+        nargs = 1,
+        desc = "Set colorscheme accent",
+      })
     end,
   },
 }
