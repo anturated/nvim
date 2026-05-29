9360ed9e4aa09971a6febfdef6c6e8f0cef08798
Desant <desant@anturated.dev>
2026-05-30 00:39:31 +0200

    feat: add accent persistence

--- config/lua/newydd/plugins/colorscheme.lua
+++ config/lua/newydd/plugins/colorscheme.lua
@@ -4,12 +4,30 @@ return {
     priority = 1000,
     after = function()
       local evergarden = require("evergarden")
+      local accent_file = vim.fn.stdpath("data") .. "/newydd_accent"
+
+      local function save_accent(accent)
+        local f = io.open(accent_file, "w")
+        if f then
+          f:write(accent)
+          f:close()
+        end
+      end
+
+      local function load_accent()
+        local f = io.open(accent_file, "r")
+        if f then
+          local accent = f:read("*l")
+          f:close()
+          return accent
+        end
+      end
 
       local opts = {
         editor = { transparent_background = true },
         theme = {
           variant = "winter",
-          accent = "lime",
+          accent = load_accent() or "lime",
         },
       }
 
@@ -19,6 +37,7 @@ return {
       vim.api.nvim_create_user_command("NewyddAccent", function(opts_)
         local accent = opts_.args
         opts.theme.accent = accent
+        save_accent(accent)
 
         require("evergarden").setup(opts)
         vim.cmd("colorscheme evergarden")
